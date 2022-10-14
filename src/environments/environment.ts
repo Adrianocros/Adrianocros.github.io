cc35d75ec0589be2f301db9c106592d0f702d71e
Adriano Crosoletto <=>
2022-10-14 13:47:19 -0300

    ajuste porta conexao

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   //baseUrl: "https://api-systemos.herokuapp.com",
-  baseUrl: "http://localhost:8080",
+  baseUrl: "http://localhost:8080/os",
 };
 
 /*
